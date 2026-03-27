c505968c9655848aa18938e74736aa75f734d57c
sam <scb40@sussex.ac.uk>
2026-03-27 12:41:47 +0000

    Renamed and fixed SQL files SQL/CreateRecordConsetLog.sql -> SQL/MakeRecordConsetLog.sql SQL/CreateTIprobeIO.sql -> SQL/MakeTIprobeIO.sql SQL/CreateTItrainIO.sql -> SQL/MakeTItrainIO.sql Removed State variable from SQL/MakeUnfocuses.sql

--- SQL/MakeUnfocuses.sql
+++ SQL/MakeUnfocuses.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `c01_DataStore`.`Unfocuses` (
     `SubjectId` TEXT NULL DEFAULT NULL,
-    `State` INT NULL DEFAULT NULL,
     `Href` TEXT NULL DEFAULT NULL,
     `DateTime_Unfocus` DATETIME NULL DEFAULT NULL
 ) ENGINE = InnoDB;
